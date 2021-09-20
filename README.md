2a7d80f8bce70617d3f1e3af8847d95edc78e1ad
Matheus Antônio <mm0389175@gmail.com>
2021-09-20 12:31:34 -0300

    Commit Menu atualizações

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OnePage
+
+20/09 - Atualização das funções de click do menu.
---
+++ js/menu.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    $('#btn-menu').click(function(e){
+        e.preventDefault()
+        $('#menu').toggle('show')
+    })
+
+    // Criar uma função para monitorar os cliques nos links
+    $('a').click(function(e){
+        e.preventDefault()
+
+        // $('#home').hide()
+        // $('#about').hide()
+        // $('#action').hide()
+        // $('#contact').hide()
+
+        // capturar qual é a página que desejo abrir
+        let url = $(this).attr('href')
+        $('#'+url).show()
+
+        switch(url){
+            case 'home':
+                $('#home').show()
+                $('#about').hide()
+                $('#action').hide()
+                $('#contact').hide()
+            break
+
+            case 'about':
+                $('#about').show()
+                $('#home').hide()
+                $('#action').hide()
+                $('#contact').hide()
+            break
+
+            case 'action':
+                $('#action').show()
+                $('#home').hide()
+                $('#about').hide()
+                $('#contact').hide()
+            break
+
+            case 'contact':
+                $('#contact').show()
+                $('#home').hide()
+                $('#about').hide()
+                $('#action').hide()
+            break
+
+            case 'close':
+                $('#home').hide()
+                $('#about').hide()
+                $('#action').hide()
+                $('#contact').hide()
+                $('#close').hide()
+            break
+            
+            default: alert('Link não encontrado');
+        }
+    })
+})
